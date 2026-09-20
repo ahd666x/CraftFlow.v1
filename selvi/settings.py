34227600d60e6fded1b0dd37da93907c13b713c6
ahd666x <ahd666x@gmail.com>
2026-09-20 10:38:18 +0330

    1405-06-29-10:38

--- selvi/settings.py
+++ selvi/settings.py
@@ -135,8 +135,10 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [BASE_DIR/"static"]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STORAGES = {
+    "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
+    "staticfiles": {"BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"},
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
